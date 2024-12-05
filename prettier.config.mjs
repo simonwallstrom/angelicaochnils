6693e871249af2bb99d58c8551a6b9a8478302c6
simonwallstrom <simon.wallstrom@gmail.com>
2024-12-05 23:15:54 +0100

    Init

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+}
+
+export default config
